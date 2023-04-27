1a5de894731c2fe0dff17ece64735964b26fc7ae
Kevin ZITNIK <zitnik.kevin@gmail.com>
2023-04-27 13:23:27 +0200

    feat : Add static attribute and static method feat : Add a function calculPrice

--- classes/Réservation.php
+++ classes/Réservation.php
@@ -6,6 +6,8 @@ class Réservation {
     private Client $_client;
     private Chambre $_room;
 
+    private static int $_reservationCount = 0;
+
     public function __construct(string $dateArrived, string $dateDeparture, Client $client, Chambre $room) {
         $this->_dateArrived = new DateTime($dateArrived);
         $this->_dateDeparture = new DateTime($dateDeparture);
@@ -14,12 +16,32 @@ class Réservation {
 
         $this->_client->addReservation($this);
         $this->_room->addReservation($this); 
+
+        self::$_reservationCount++;
     }
 
     public function week() {
         return " du ".$this->_dateArrived->format("d.m.Y")." au ".$this->_dateDeparture->format("d.m.Y")."<br/>";
     }
     
+    public function calculPrice(){ // Calcul du prix en partant du nombre de jours réservés. 
+
+        $reservationDuration = $this->_dateArrived->diff($this->_dateDeparture); // Calcul du nombres de jours réservés. 
+
+        if ($reservationDuration->d == 0)  { // Si le nombre de jours réservés est de 0, alors on sort juste le prix de la chambre.
+            $price = $this->getRoom()->getPrice();
+        } 
+        else {
+            $price = $this->getRoom()->getPrice() * $reservationDuration->d; // Sinon, on multiplie par le nombre de jours sinon il y a multiplication par 0.
+        }
+        
+        return $price ;
+    }
+
+    public static function getReservationCount() {
+        echo self::$_reservationCount++;
+    }
+
     public function getDateArrived(){
         return $this->_dateArrived;
     }
@@ -52,5 +74,4 @@ class Réservation {
         $this->_room = $room;
     }
 
-    
 }
